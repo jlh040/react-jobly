47d9537d630155c1e5d570c3fc20b73e13f318c5
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-19 20:34:04 -0700

    imported components into the Routes file

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Homepage from './Homepage';
+import Companies from './Companies';
+import Company from './Company';
+import Jobs from './Jobs';
+import Profile from './Profile';
+import SignUp from './SignUp';
+import Login from './Login';
 
 const Routes = () => {
   return (
@@ -26,9 +33,6 @@ const Routes = () => {
           </Route>
           <Route exact path="/login">
             <Login />
-          </Route>
-          <Route>
-            <NotFoundPage />
           </Route>
         </Switch>
       </BrowserRouter>
