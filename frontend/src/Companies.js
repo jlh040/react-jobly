e78b44795b8164f9d6669ebc901a8f5310553b7e
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-20 14:46:51 -0700

    data is now able to be displayed on screen

--- frontend/src/Companies.js
+++ frontend/src/Companies.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import JoblyApi from './api';
 
 const Companies = () => {
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const [companies, setCompanies] = useState([]);
+  useEffect(() => {
+    const getCompanies = async () => {
+      const res = await JoblyApi.getAllCompanies();
+      setCompanies(res);
+      setHasLoaded(true);
+    };
+    getCompanies()
+  }, [])
   return (
     <div>
-      <h1>Companies!</h1>
+      {hasLoaded ? companies.map(company => <div>{company.name}</div>) : <h1>Loading...</h1>}
     </div>
   )
 };
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -56,4 +56,4 @@ JoblyApi.token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZ" +
     "SI6InRlc3R1c2VyIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTU5ODE1OTI1OX0." +
     "FtrMwBQwe6Ue-glIFgz_Nf8XxRT2YecFCiSpYL0fCXc";
 
-module.exports = JoblyApi;
+export default JoblyApi;
