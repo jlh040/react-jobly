170a054fc72eaf5d4936eb3c351a7570ac678465
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-19 20:22:53 -0700

    created routes and added placeholder components

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -2,7 +2,38 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const Routes = () => {
-
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Homepage />
+          </Route>
+          <Route exact path="/companies">
+            <Companies />
+          </Route>
+          <Route exact path="/companies/:handle">
+            <Company />
+          </Route>
+          <Route exact path="/jobs">
+            <Jobs />
+          </Route>
+          <Route exact path="/profile">
+            <Profile />
+          </Route>
+          <Route exact path="/signup">
+            <SignUp />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route>
+            <NotFoundPage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
 };
 
 export default Routes;
