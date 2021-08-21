baad107f93e72ad791f747558fc21c85d8509eac
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-20 18:11:47 -0700

    users can now search for jobs by title

--- frontend/src/JobsList.js
+++ frontend/src/JobsList.js
@@ -2,11 +2,17 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import { Container } from 'reactstrap';
 import JobCard from './JobCard';
+import JoblyApi from './api';
 
 const JobsList = ({ jobs, setJobs }) => {
+  const handleSubmit = async (e, formData) => {
+    e.preventDefault();
+    const jobs = await JoblyApi.searchForJobs(formData.search);
+    setJobs(jobs);
+  }
   return (
     <Container >
-      <SearchBar />
+      <SearchBar handleSubmit={handleSubmit} />
       {jobs.map(job => <JobCard job={job} displayCompany />)}
     </Container>
   )
