bc10e0ce527e0142257c96d42413dec0d5aea833
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-21 19:37:33 -0700

    when a user signs up the navbar changes accordingly

--- frontend/src/hooks/useSignUp.js
+++ frontend/src/hooks/useSignUp.js
@@ -1,12 +1,15 @@
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const useSignUp = (JoblyApi) => {
+  const history = useHistory();
   const [token, setToken] = useState();
   const handleSignUp = async (e, formData) => {
     e.preventDefault();
     const respToken = await JoblyApi.signUp(formData);
     JoblyApi.token = respToken;
     setToken(respToken);
+    history.push('/');
   };
 
   return [token, handleSignUp];
