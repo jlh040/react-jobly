c4a9935142308b42776543845aba74f94f7338c3
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-21 21:02:06 -0700

    users now see a different homepage message depending on if they are logged in or not

--- frontend/src/Homepage.js
+++ frontend/src/Homepage.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import UserContext from './userContext';
+import { Jumbotron } from 'reactstrap';
+import './Homepage.css';
 
 const Homepage = () => {
+  const { user } = useContext(UserContext);
   return (
-    <div>
-      <h1>Homepage!</h1>
+    <div className="Homepage">
+      <Jumbotron className="Homepage-jumbotron">
+        <h1 className="display-3">Jobly</h1>
+        <p className="lead">Your go-to place for the newest jobs.</p>
+        {user && (
+          <>
+            <hr className="my-2" />
+            <h3>Welcome back {user.firstName}!</h3>
+          </>
+        )}
+      </Jumbotron>
     </div>
   )
 };
