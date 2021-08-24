68fa638db3e53843527ea4ebee20f967a866e981
Jeff Hall <jeff.leon.hall@gmail.com>
2021-08-24 12:54:50 -0700

    deleted the concurrently dependency and corresponding files

--- start-server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const app = require('./backend/app');
-const { PORT } = require('./backend/config');
-
-app.listen(PORT, function () {
-  console.log(`Started on http://localhost:${PORT}`);
-});
